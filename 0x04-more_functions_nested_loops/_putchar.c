5f476e54170bbaa43ccb5aa26847ab0085644e12
Johnsela <johnstranja@yahoo.com>
2023-06-23 16:31:12 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar.c - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set apprropriately.
+ */
+int _putchar.c(char c)
+{
+	return (write(1, &c, 1));
+}
